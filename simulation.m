86be3f4aeb21e8df43b95568fb7f1c15eb7093a1
Yehu Chen <chenyehu@wustl.edu>
2023-11-18 15:36:44 -0600

    rerun sim1

--- simulation.m
+++ simulation.m
@@ -1,8 +1,8 @@
 if ~exist('SEED','var')
     % simulation settings
     SEED = 10;
-    data_name = "twoDplane";
-    N = 600;
+    data_name = "Friedman";
+    N = 100;
     test_anchor = 0;
 end
 
@@ -33,16 +33,16 @@ simulate_data;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % difference-in-mean estimator with complete independent assumption
-BIN=10;
+BIN=2;
 diff_in_mean;
 D = (size(train_x,2))/2;
-dim_mu = repmat(dim_mu',N,1)';
-dim_std = repmat(dim_std',N,1)';
+% dim_mu = repmat(dim_mu',N,1)';
+% dim_std = repmat(dim_std',N,1)';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % dgp effects
 [dgp_effects,~]=gp_AMCE(dgp_dy, dgp_dy*0, data_name, train_x);
-dgp_effects = reshape(dgp_dy(:,1:D),[],1)';
+dgp_effects = mean(dgp_dy(:,1:D));
 % [dgp_effects,~]=gp_point_est(BIN,raw_x,dgp_dy,dgp_dy.*0);
 % dgp_effects = mean(dgp_dy(:,1:D));
 
@@ -57,8 +57,8 @@ gp_pref_grad;
 % gp_point_mu = mean(dy_mu(:,1:D));
 % gp_point_std = sqrt(sum(dy_std(:,1:D).^2))./N;
 [gp_point_mu,gp_point_std] = gp_AMCE(dy_mu,dy_std,data_name,train_x);
-gp_point_mu = reshape(dy_mu,1,[]);
-gp_point_std = reshape(dy_std,1,[]);
+gp_point_mu = mean(dy_mu);
+gp_point_std = sqrt(mean(dy_std.^2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % gp preference learning GMM effect
@@ -67,8 +67,8 @@ gp_point_std = reshape(dy_std,1,[]);
 % gp_GMM_mu= sqrt(sum(sigma_GMM_avg(:,1:D).^2))./N;
 [gp_GMM_mu,gp_GMM_std] = gp_AMCE(mu_GMM_avg,sigma_GMM_avg,data_name,train_x);
 % disp(mean((dgp_effects>=gp_GMM_mu-2*gp_GMM_std) & (dgp_effects<=gp_GMM_mu+2*gp_GMM_std)));
-gp_GMM_mu = reshape(mu_GMM_avg,1,[]);
-gp_GMM_std = reshape(sigma_GMM_avg, 1,[]);
+gp_GMM_mu = mean(mu_GMM_avg);
+gp_GMM_std = sqrt(mean(sigma_GMM_avg.^2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 D = numel(dgp_effects);
