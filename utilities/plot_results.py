10505e4e910ba9c0b105b229ba4a49766021ed3d
Yehu Chen <chenyehu@wustl.edu>
2023-11-20 11:54:02 -0600

    rerun sim1

--- utilities/plot_results.py
+++ utilities/plot_results.py
@@ -10,7 +10,7 @@ NS = [100, 200, 300, 400, 500, 600]
 MEASURES = ["RMSE","CORRELATION", "COVERAGE","LL"]
 
 def main(args):
-    MODELS = ["lm", "diffinmean", "gppoint", "gpGMM"]
+    MODELS = ["diffinmean", "gppoint", "gpGMM"] # "lm"
     MAXSEED = int(args["seed"])
     TA = int(args["TA"])
     
@@ -47,9 +47,9 @@ def main(args):
                     results[2,i,j,k,SEED-1] = COVERAGE
                     results[3,i,j,k,SEED-1] = LL
     
-    MODELS = ["lm", "diff-in-mean", "gp-GMM-1", "gp_GMM-10"]
+    MODELS = ["diff-in-mean", "gp-GMM-1", "gp_GMM-10"]
     fig, ax = plt.subplots(nrows=len(DATA_NAMES), ncols=len(MEASURES), figsize=(15, 8), dpi=100)
-    colors = ["darkkhaki","limegreen", "red", "blue"]
+    colors = ["limegreen", "red", "blue"]
     for i in range(len(DATA_NAMES)):
         for m in range(len(MEASURES)):
             if i==0:
