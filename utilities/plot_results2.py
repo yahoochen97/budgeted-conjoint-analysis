e51a24758dc8d6babbfbc77d69674d34e800af39
Yehu Chen <chenyehu@wustl.edu>
2024-04-22 10:09:15 -0500

    sim2

--- utilities/plot_results2.py
+++ utilities/plot_results2.py
@@ -8,7 +8,7 @@ sys.path.append("./utility")
 
 DATA_NAMES = ["twoDplane", "Friedman"]
 N = 700
-TOTAL_SIZES = [25*i+75 for i in range(6)]
+TOTAL_SIZES = [25*i+50 for i in range(6)]
 MEASURES = ["RMSE","CORRELATION", "COVERAGE","LL"]
 
 def main(args):
