abe32c9c63b03e92032778266c0173ee9fe0efd9
Yehu Chen <chenyehu@wustl.edu>
2024-04-29 11:31:29 -0500

    sim2

--- utilities/plot_results2.py
+++ utilities/plot_results2.py
@@ -67,9 +67,9 @@ def main(args):
                 ax[i,m].set_ylabel(DATA_NAMES[i])
             bplots = []
             for k in range(len(MODELS)):
-                tmp = results[m,i,:,k,:]
                 tmp = [tmp[j,:] for j in range(len(TOTAL_SIZES))]
                 if effect_type=="pop":
+                    tmp = results[m,i,:,k,:]
                     tmp = [(tmp[j,:]-np.mean(tmp[j,:]))/np.sqrt(MAXSEED)+np.mean(tmp[j,:]) for j in range(len(TOTAL_SIZES))]  
                 bplot = ax[i,m].boxplot(tmp, positions=4+4*np.arange(len(TOTAL_SIZES))+(k-1)*0.75, showfliers=False,\
                                     patch_artist=True, widths=0.45)
