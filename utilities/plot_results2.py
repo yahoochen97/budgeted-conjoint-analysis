ef68ea35b7b6e28a76381aa1768c82e3b1a084b7
Yehu Chen <chenyehu@wustl.edu>
2024-04-29 13:51:31 -0500

    sim2

--- utilities/plot_results2.py
+++ utilities/plot_results2.py
@@ -104,7 +104,7 @@ def main(args):
         'figure.figsize': [10, 6]
     }
     plt.rcParams.update(params)
-    fig.subplots_adjust(left=0.05, bottom=0.035, right=0.99, top=0.96, wspace=0.12)
+    fig.subplots_adjust(left=0.08, bottom=0.05, right=0.99, top=0.99, wspace=0.12)
     if effect_type=="pop":
         plt.savefig("./results2/simulation2_plot.pdf", format="pdf", dpi=100)
     else:
@@ -167,7 +167,7 @@ def compare_ACC(args):
         'figure.figsize': [10, 6]
     }
     plt.rcParams.update(params)
-    fig.subplots_adjust(left=0.05, bottom=0.045, right=0.99, top=0.94, wspace=0.12)
+    fig.subplots_adjust(left=0.05, bottom=0.035, right=0.99, top=0.96, wspace=0.12)
     plt.savefig("./results2/simulation2_ACC.pdf", format="pdf", dpi=100)
 
 if __name__ == "__main__":
